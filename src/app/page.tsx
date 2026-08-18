9411867761459af3c93d18cab80b4db50064981a
Ethan <ethan.yang@25sprout.com>
2026-08-18 13:44:59 -0400

    add email newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,58 +19,74 @@ export default function Home() {
 
   return (
     <main className="">
+      {/* Background */}
       <div className="-z-10 rounded-full animate-pulse absolute top-1/2 left-1/2 -translate-x-1/3 -translate-y-1/3 size-[30vw] bg-sky-200/30 blur-3xl" />
       <div className="-z-10 rounded-full animate-pulse absolute bottom-1/2 right-1/2 translate-x-1/3 translate-y-1/3 size-[30vw] bg-lime-200/30 blur-3xl" />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-lg border p-6 rounded-2xl backdrop-blur-2xl">
-        <div className="flex items-center gap-4">
-          <div className="shrink-0">
-            <Image
-              src={logo}
-              alt="Emory Hacks Logo"
-              width={170}
-              className="object-contain"
-            />
-          </div>
-          <div className="grow">
-            <h1 className="mb-4 whitespace-nowrap">
-              Emory Hacks
-              <br />
-              2026 Fall
-            </h1>
-            <h3 className="text-muted-foreground">11/13 - 11/15</h3>
-          </div>
-          <div className="w-20 flex flex-col justify-between text-muted-foreground/40 text-3xl font-black text-right">
-            {isMounted ? (
-              <Countdown
-                date={new Date(DDAY)}
-                intervalDelay={0}
-                precision={3}
-                renderer={({ days, hours, minutes, seconds }) => {
-                  return (
-                    <>
-                      <p className="font-mono">{days}</p>
-                      <p className="font-mono">{zeroPad(hours, 2)}</p>
-                      <p className="font-mono">{zeroPad(minutes, 2)}</p>
-                      <p className="font-mono">{zeroPad(seconds, 2)}</p>
-                    </>
-                  );
-                }}
+
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-5 w-full max-w-lg">
+        {/* MAIN CARD */}
+        <div className="w-full border p-6 rounded-2xl backdrop-blur-2xl">
+          <div className="flex items-center gap-4">
+            <div className="shrink-0">
+              <Image
+                src={logo}
+                alt="Emory Hacks Logo"
+                width={170}
+                className="object-contain"
               />
-            ) : (
-              <>
-                <p className="font-mono">--</p>
-                <p className="font-mono">--</p>
-                <p className="font-mono">--</p>
-                <p className="font-mono">--</p>
-              </>
-            )}
+            </div>
+            <div className="grow">
+              <h1 className="mb-4 whitespace-nowrap">
+                Emory Hacks
+                <br />
+                2026 Fall
+              </h1>
+              <h3 className="text-muted-foreground">11/13 - 11/15</h3>
+            </div>
+            <div className="w-20 flex flex-col justify-between text-muted-foreground/40 text-3xl font-black text-right">
+              {isMounted ? (
+                <Countdown
+                  date={new Date(DDAY)}
+                  intervalDelay={0}
+                  precision={3}
+                  renderer={({ days, hours, minutes, seconds }) => {
+                    return (
+                      <>
+                        <p className="font-mono">{days}</p>
+                        <p className="font-mono">{zeroPad(hours, 2)}</p>
+                        <p className="font-mono">{zeroPad(minutes, 2)}</p>
+                        <p className="font-mono">{zeroPad(seconds, 2)}</p>
+                      </>
+                    );
+                  }}
+                />
+              ) : (
+                <>
+                  <p className="font-mono">--</p>
+                  <p className="font-mono">--</p>
+                  <p className="font-mono">--</p>
+                  <p className="font-mono">--</p>
+                </>
+              )}
+            </div>
           </div>
         </div>
-        {/* <Button asChild size="lg" className="w-full mt-5">
-          <Link href="/register">Register</Link>
-        </Button> */}
+
+        {/* EMAIL NEWSLETTER */}
+        <p className="text-sm text-muted-foreground font-medium">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScvSGDzvizF7aU1PFN8a-Fwrlw_z91hhNNZ6hY3TODnHiYHYw/viewform?usp=publish-editor"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-foreground underline underline-offset-4 hover:opacity-70 transition-opacity"
+          >
+            Sign up
+          </a>{" "}
+          to our email newsletter!
+        </p>
       </div>
 
+      {/* SOCIALS SECTION */}
       <div className="fixed bottom-8 w-full flex flex-col items-center">
         <p className="text-sm text-muted-foreground mb-3 font-medium">
           Follow us on our socials!
